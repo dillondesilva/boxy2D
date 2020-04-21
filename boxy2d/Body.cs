06403c2fe2a01165e35922d46c62f48983702da2
Dillon de Silva <dillon.desilva@geeksdiary.com>
2020-04-21 16:37:38 +1000

    Initial Commit. Basic set up of physics engine

---
+++ boxy2d/Body.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Body
+{
+    public class MotionCalculations
+    {
+        public float GetNetForce(float Mass, float Acceleration)
+        {
+            float NetForce = Mass * Acceleration;
+            return NetForce;
+        }
+    }
+
+    public class Box : MotionCalculations
+    {
+        public float Mass;
+        public float Acceleration;
+        public float Position;
+    }
+}
